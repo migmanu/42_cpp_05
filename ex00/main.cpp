00f1f438b17a1779169804e2ec47596641c0599d
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-04-14 17:07:41 +0200

    finish ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,4 +1,29 @@
-int	main(void)
+#include "Bureaucrat.hpp"
+#include <exception>
+#include <iostream>
+#include <ostream>
+
+int main(void)
 {
+	try
+	{
+		Bureaucrat manu("manu", 14);
+		for (int i = 10; i > 0; i--)
+		{
+			std::cout << manu;
+			manu.incrementGrade();
+		}
+		Bureaucrat src("unam", 141);
+		Bureaucrat unam(src);
+		for (int i = 10; i > 0; i--)
+		{
+			std::cout << unam;
+			unam.decrementGrade();
+		}
+	}
+	catch (std::exception &exception)
+	{
+		std::cerr << "* Exception caught: " << exception.what() << " *" << std::endl;
+	}
 	return 0;
 }
